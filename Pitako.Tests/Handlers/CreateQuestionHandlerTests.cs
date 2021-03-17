6f764115b5fd8b16d948b22a5d390856fa043a26
Guilherme Rodrigues Filenga <guilherme.filenga@gmail.com>
2021-03-16 21:08:38 -0300

    Testando o QuestionHandler

--- Pitako.Tests/Handlers/CreateQuestionHandlerTests.cs
+++ Pitako.Tests/Handlers/CreateQuestionHandlerTests.cs
@@ -3,29 +3,38 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Pitako.Domain.Commands;
 using Pitako.Domain.Entities;
 using Pitako.Domain.Handlers;
+using Pitako.Tests.Repositories;
 
 namespace Pitako.Tests.HandlerTests
 {
     [TestClass]
     public class CreateQuestionHandlerTests
     {
+        private readonly CreateQuestionCommand _validCommand = new CreateQuestionCommand(
+                    "Vida pessoal",
+                    "Preciso de uma opinião na minha vida pessoal",
+                    DateTime.Now,
+                    new User("Guilherme", "gui.filenga@hotmail.com", "1234567"));
         private readonly CreateQuestionCommand _invalidCommand = new CreateQuestionCommand(
                     "",
                     "",
                     DateTime.Now,
                     new User("", "", ""));
 
+        private readonly QuestionHandler _handler = new QuestionHandler(new FakeQuestionRepository());
+
         [TestMethod]
         public void ShouldStopExcecutionWhenCommandIsNotValid()
         {
-            var handler = new QuestionHandler(null);
-            Assert.AreEqual(handler.Valid, false);
+            var result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+            Assert.AreEqual(result.Success, false);
         }
 
         [TestMethod]
         public void ShouldExcecuteWhenCommandIsValid()
         {
-            Assert.Fail();
+            var result = (GenericCommandResult)_handler.Handle(_validCommand);
+            Assert.AreEqual(_handler.Valid, true);
         }
     }
 }
---
+++ Pitako.Tests/Repositories/FakeQuestionRepository.cs
@@ -0,0 +1,16 @@
+using Pitako.Domain.Entities;
+using Pitako.Domain.Repositories;
+
+namespace Pitako.Tests.Repositories
+{
+    public class FakeQuestionRepository : IQuestionRepository
+    {
+        public void Create(Question question)
+        {
+        }
+
+        public void Update(Question question)
+        {
+        }
+    }
+}
