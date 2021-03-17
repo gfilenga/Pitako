5364e96fe33fa04a10824b4d963140838f5a8681
Guilherme Rodrigues Filenga <guilherme.filenga@gmail.com>
2021-03-17 18:12:13 -0300

    Testes iniciais da AnswerEntity

---
+++ Pitako.Tests/Entities/AnswerTests.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pitako.Domain.Commands;
+using Pitako.Domain.Entities;
+
+namespace Pitako.Tests.Entities
+{
+    [TestClass]
+    public class AnswerTests
+    {
+        private readonly Answer _answer = new Answer(
+                "Descrição atualizada",
+                new User(
+                    "Guilherme",
+                    "gui.filenga@hotmail.com",
+                    "1234567"),
+                new Question(
+                    "Vida Pessoal",
+                    "Preciso de ajuda",
+                    new User(
+                        "Eduardo",
+                        "edu.filenga@hotmail.com",
+                        "1234567")
+                )
+        );
+
+        [TestMethod]
+        public void ShouldStartAAnswerAsActiveTrue()
+        {
+            Assert.AreEqual(_answer.Active, true);
+        }
+
+        [TestMethod]
+        public void ShouldToggleTheActiveProperty()
+        {
+            _answer.ToogleStatus();
+            Assert.AreEqual(_answer.Active, false);
+        }
+
+    }
+}
