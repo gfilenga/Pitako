fb784ba2bbbc1b5e535d6dc17a0cd954c0d62ba5
Guilherme Rodrigues Filenga <guilherme.filenga@gmail.com>
2021-03-17 23:48:53 -0300

    Feito testes preliminares

---
+++ Pitako.Tests/Commands/Answer/CreateAnswerTests.cs
@@ -0,0 +1,38 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pitako.Domain.Commands;
+using Pitako.Domain.Entities;
+
+namespace Pitako.Tests.Command
+{
+    [TestClass]
+    public class CreateAnswerTests
+    {
+        private readonly CreateAnswerCommand _validCommand = new CreateAnswerCommand(
+            "Descrição da pergunta",
+            DateTime.Now,
+            new User("Guilherme", "gui.filenga@hotmail.com", "1234567"),
+            new Question("Helpppp", "Me ajudem com isso", new User("Eduardo", "edu.filenga@hotmail.com", "12345679"))
+        );
+        private readonly CreateAnswerCommand _invalidCommand = new CreateAnswerCommand(
+            "",
+            DateTime.Now,
+            new User("", "", ""),
+            new Question("", "", new User("", "", ""))
+        );
+
+        [TestMethod]
+        public void ShouldValidateWhenCommandIsValid()
+        {
+            _validCommand.Validate();
+            Assert.AreEqual(_validCommand.Valid, true);
+        }
+        [TestMethod]
+        public void ShouldNotValidateWhenCommandIsNotValid()
+        {
+
+            _invalidCommand.Validate();
+            Assert.AreEqual(_invalidCommand.Invalid, true);
+        }
+    }
+}
