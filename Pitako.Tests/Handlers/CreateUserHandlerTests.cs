e4e98e72a288fb9c9da5bf750d20ad0a6b25107d
Guilherme Rodrigues Filenga <guilherme.filenga@gmail.com>
2021-03-18 17:43:37 -0300

    test: user handler testes

---
+++ Pitako.Tests/Handlers/CreateUserHandlerTests.cs
@@ -0,0 +1,39 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pitako.Domain.Commands;
+using Pitako.Domain.Entities;
+using Pitako.Domain.Handlers;
+using Pitako.Tests.Repositories;
+
+namespace Pitako.Tests.Handlers
+{
+    [TestClass]
+    public class CreateUserHandlerTests
+    {
+        private readonly UserHandler _handler = new UserHandler(new FakeUserRepository());
+        private readonly CreateUserCommand _validCommand = new CreateUserCommand(
+            "Guilherme",
+            "gui.filenga@hotmail.com",
+            "1234567"
+        );
+        private readonly CreateUserCommand _invalidCommand = new CreateUserCommand(
+                "",
+                "",
+                ""
+        );
+
+        [TestMethod]
+        public void ShouldExcecuteWhenCommandIsValid()
+        {
+            var result = (GenericCommandResult)_handler.Handle(_validCommand);
+            Assert.AreEqual(result.Success, true);
+        }
+
+        [TestMethod]
+        public void ShouldStopExcecutionWhenCommandIsNotValid()
+        {
+            var result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+            Assert.AreEqual(result.Success, false);
+        }
+    }
+}
